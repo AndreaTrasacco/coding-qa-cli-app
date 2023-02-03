26fb592d36131999a7b97c993a3cffbd18dce46d
Giovanni <giova.gb00@gmail.com>
2023-02-03 18:18:59 +0100

    Service Locator

--- src/main/java/it/unipi/lsmsd/coding_qa/service/ServiceLocator.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/service/ServiceLocator.java
@@ -1,4 +1,29 @@
 package it.unipi.lsmsd.coding_qa.service;
 
+import it.unipi.lsmsd.coding_qa.service.implementation.AggregationsServiceImpl;
+import it.unipi.lsmsd.coding_qa.service.implementation.QuestionServiceImpl;
+import it.unipi.lsmsd.coding_qa.service.implementation.SuggestionsServiceImpl;
+import it.unipi.lsmsd.coding_qa.service.implementation.UserServiceImpl;
+
 public class ServiceLocator {
+    private static AggregationsService aggregationsService = new AggregationsServiceImpl();
+    private static QuestionService questionService = new QuestionServiceImpl();
+    private static SuggestionsService suggestionService = new SuggestionsServiceImpl();
+    private static UserService userService = new UserServiceImpl();
+
+    public static AggregationsService getAggregationsService() {
+        return aggregationsService;
+    }
+
+    public static QuestionService getQuestionService() {
+        return questionService;
+    }
+
+    public static SuggestionsService getSuggestionService() {
+        return suggestionService;
+    }
+
+    public static UserService getUserService() {
+        return userService;
+    }
 }
