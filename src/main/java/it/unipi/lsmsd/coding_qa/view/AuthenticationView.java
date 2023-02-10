863322331463507cc8ae10ef887c3c12c1ab49fd
Andrea1900 <66999436+Andrea1900@users.noreply.github.com>
2023-02-10 18:20:18 +0100

    Modified class AuthenticationView

--- src/main/java/it/unipi/lsmsd/coding_qa/view/AuthenticationView.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/view/AuthenticationView.java
@@ -20,7 +20,7 @@ public class AuthenticationView {
             System.out.println("\t[2] Log in");
             System.out.println("\t[3] Go to main menu without logging in");
             System.out.println("\t[4] Exit");
-            System.out.print("Input: ");
+            System.out.println("Input: ");
             choice = scanner.nextInt();
         } while (choice < 1 && choice > 4);
         if (choice == 4)
@@ -31,37 +31,37 @@ public class AuthenticationView {
     public UserRegistrationDTO signUp() { // We assume the user inputs the correct data, so minimal data validation
         UserRegistrationDTO userDTO = new UserRegistrationDTO();
         System.out.println("############################################## SIGN UP #############################################");
-        System.out.print("\t* Nickname: ");
+        System.out.println("* Nickname: ");
         userDTO.setNickname(scanner.nextLine().replaceAll(" ", "_"));
-        System.out.print("\t* Password: ");
+        System.out.println("* Password: ");
         userDTO.setEncPassword(scanner.nextLine());
-        System.out.print("\t* Full Name: ");
+        System.out.println("* Full Name: ");
         userDTO.setFullName(scanner.nextLine());
-        System.out.print("\t* Year of birth date: ");
+        System.out.println("* Year of birth date: ");
         int year = scanner.nextInt();
-        System.out.print("\t* Month of birth date: ");
+        System.out.println("* Month of birth date: ");
         int month = scanner.nextInt();
-        System.out.print("\t* Day of birth date: ");
+        System.out.println("* Day of birth date: ");
         int day = scanner.nextInt();
         userDTO.setBirthdate(new Date(year - 1900, month - 1, day));
         scanner.nextLine(); // to read '\n' leaved in buffer after nextInt() TODO ATTENZIONE
-        System.out.println("\tPossible Countries: " + Arrays.toString(Constants.COUNTRIES.toArray()));
+        System.out.println("Possible Countries: " + Arrays.toString(Constants.COUNTRIES.toArray()));
         String country;
         do {
-            System.out.print("\t* Country: ");
+            System.out.println("* Country: ");
             country = scanner.nextLine();
         } while (!Constants.COUNTRIES.contains(country));
         userDTO.setCountry(country);
-        System.out.print("\t* Your website URL (Press Enter if you don't have any): ");
+        System.out.println("* Your website URL (Press Enter if you don't have any): ");
         userDTO.setWebsite(scanner.nextLine());
         return userDTO;
     }
 
     public UserLoginDTO login() {
         System.out.println("############################################## LOG IN ##############################################");
-        System.out.print("\t* Nickname: ");
+        System.out.println("* Nickname: ");
         String nickname = scanner.nextLine().replaceAll(" ", "_");
-        System.out.print("\t* Password: ");
+        System.out.println("* Password: ");
         String password = scanner.nextLine();
         return new UserLoginDTO(nickname, password);
     }
