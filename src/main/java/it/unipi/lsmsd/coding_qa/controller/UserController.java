acf399d627b73f64d861da261e074804f69b15c1
nicolosalti <nicolo.salti@hotmail.it>
2023-02-11 19:07:23 +0100

    Corrected QuestionView

--- src/main/java/it/unipi/lsmsd/coding_qa/controller/UserController.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/controller/UserController.java
@@ -1,9 +1,11 @@
 package it.unipi.lsmsd.coding_qa.controller;
 
 import it.unipi.lsmsd.coding_qa.dto.*;
+import it.unipi.lsmsd.coding_qa.service.AnswerService;
 import it.unipi.lsmsd.coding_qa.service.QuestionService;
 import it.unipi.lsmsd.coding_qa.service.ServiceLocator;
 import it.unipi.lsmsd.coding_qa.service.UserService;
+import it.unipi.lsmsd.coding_qa.utils.Constants;
 import it.unipi.lsmsd.coding_qa.view.MainView;
 import it.unipi.lsmsd.coding_qa.view.QuestionView;
 import it.unipi.lsmsd.coding_qa.view.UserView;
@@ -11,6 +13,7 @@ import it.unipi.lsmsd.coding_qa.view.UserView;
 public class UserController {
     private UserService userService;
     private QuestionService questionService;
+    private AnswerService answerService;
     private UserView userView = new UserView();
     private QuestionView questionView = new QuestionView();
     private MainView mainView = new MainView();
@@ -36,6 +39,7 @@ public class UserController {
     private void browseYourQuestion() {
         try {
             int page = 1;
+            int pageAns = 1;
             do{
                 UserDTO userDTO = authenticationController.getLoggedUser();
                 PageDTO<QuestionDTO> pageDTO = questionService.viewCreatedQuestions(userDTO.getNickname(), page);
@@ -52,7 +56,30 @@ public class UserController {
                                 questionView.addAnswer(answerDTO);
                                 break;
                             case 2: // browse answers
+                                PageDTO<AnswerDTO> pageDTOAns = answerService.getAnswersPage(pageAns, questionPageDTO.getId());
+                                if(pageDTOAns.getCounter() == 0) return;
+                                switch (questionView.menuInAnswerPageLogged()){
+                                    case 1:  // select an answer
+                                        int ansNumber = mainView.inputMessageWithPaging("Specify the answer number", pageDTO.getCounter());
+                                        switch (questionView.menuInAnswer()){
 
+                                        }
+                                        break;
+                                    case 2:  // go to the next page
+                                        if (pageDTOAns.getCounter() == Constants.PAGE_SIZE)
+                                            pageAns++;
+                                        else
+                                            mainView.showMessage("!!!! THIS IS THE LAST PAGE !!!!");
+                                        break;
+                                    case 3:  // go to the previous page
+                                        if (pageAns > 1)
+                                            pageAns--;
+                                        else
+                                            mainView.showMessage("!!!! THIS IS THE FIRST PAGE !!!!");
+                                        break;
+                                    case 4:  // go back
+                                        return;
+                                }
                                 break;
                             case 3: // delete question
                                 questionService.deleteQuestion(questionPageDTO.getId());
@@ -66,6 +93,20 @@ public class UserController {
                             case 5: // go back
                                 return;
                         }
+                    case 2: // go to the next page
+                        if (pageDTO.getCounter() == Constants.PAGE_SIZE)
+                            page++;
+                        else
+                            mainView.showMessage("!!!! THIS IS THE LAST PAGE !!!!");
+                        break;
+                    case 3: // go ot the previous page
+                        if (page > 1)
+                            page--;
+                        else
+                            mainView.showMessage("!!!! THIS IS THE FIRST PAGE !!!!");
+                        break;
+                    case 4: // go back
+                        return;
                 }
             } while (true);
         } catch (Exception e){
