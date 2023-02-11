4f93759121dde48e445a3245b290ac8eea8fa6db
Giovanni <giova.gb00@gmail.com>
2023-02-11 19:43:24 +0100

    QuestionController

--- src/main/java/it/unipi/lsmsd/coding_qa/controller/QuestionController.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/controller/QuestionController.java
@@ -1,9 +1,6 @@
 package it.unipi.lsmsd.coding_qa.controller;
 
-import it.unipi.lsmsd.coding_qa.dto.AnswerDTO;
-import it.unipi.lsmsd.coding_qa.dto.PageDTO;
-import it.unipi.lsmsd.coding_qa.dto.QuestionDTO;
-import it.unipi.lsmsd.coding_qa.dto.QuestionSearchDTO;
+import it.unipi.lsmsd.coding_qa.dto.*;
 import it.unipi.lsmsd.coding_qa.service.AnswerService;
 import it.unipi.lsmsd.coding_qa.service.QuestionService;
 import it.unipi.lsmsd.coding_qa.service.ServiceLocator;
@@ -18,6 +15,7 @@ public class QuestionController {
     private QuestionView questionView;
     private MainView mainView;
     private AdminView adminView;
+    private AuthenticationController authenticationController;
 
     public QuestionController(){
         questionService = ServiceLocator.getQuestionService();
@@ -25,6 +23,7 @@ public class QuestionController {
         questionView =  new QuestionView();
         mainView = new MainView();
         adminView = new AdminView();
+        authenticationController = new AuthenticationController();
     }
 
     // Browse e search for admin, logged, notlogged (VALUTARE SE TROPPO DUPICATO)
@@ -134,6 +133,106 @@ public class QuestionController {
         }
     }
 
+
+    private void browseYourQuestion() {
+        try {
+            int page = 1;
+            int pageAns = 1;
+            do{
+                UserDTO userDTO = authenticationController.getLoggedUser();
+                PageDTO<QuestionDTO> pageDTO = questionService.viewCreatedQuestions(userDTO.getNickname(), page);
+                if (pageDTO.getCounter() == 0) return;
+                switch(questionView.browseQuestionsMenu()){
+                    case 1: // open a question
+                        int number = mainView.inputMessageWithPaging("Specify the question number", pageDTO.getCounter());
+                        QuestionPageDTO questionPageDTO = questionService.getQuestionInfo(pageDTO.getEntries().get(number - 1).getId());
+                        mainView.view(questionPageDTO);
+                        switch (questionView.menuInQuestionPageOwner()){
+                            case 1: // add an answer
+                                AnswerDTO answerDTO = new AnswerDTO();
+                                answerDTO.setAuthor(userDTO.getNickname());
+                                questionView.addAnswer(answerDTO);
+                                break;
+                            case 2: // browse answers
+                                PageDTO<AnswerDTO> pageDTOAns = answerService.getAnswersPage(pageAns, questionPageDTO.getId());
+                                if(pageDTOAns.getCounter() == 0) return;
+                                switch (questionView.menuInAnswerPageLogged()){
+                                    case 1:  // select an answer
+                                        int ansNumber = mainView.inputMessageWithPaging("Specify the answer number", pageDTO.getCounter());
+
+                                        switch (questionView.menuInAnswer()){
+                                            case 1:  // Upvote
+                                                VoteDTO voteDTO = new VoteDTO();
+                                                voteDTO.setAnswerId(pageDTOAns.getEntries().get(ansNumber - 1).getId());
+                                                voteDTO.setVoteType(true);
+                                                voteDTO.setAnswerOwner(pageDTOAns.getEntries().get(ansNumber - 1).getAuthor());
+                                                voteDTO.setVoterId(userDTO.getId());
+                                                answerService.voteAnswer(voteDTO);
+                                                break;
+                                            case 2:  // Downvote
+                                                VoteDTO voteDTO1 = new VoteDTO();
+                                                voteDTO1.setAnswerId(pageDTOAns.getEntries().get(ansNumber - 1).getId());
+                                                voteDTO1.setVoteType(false);
+                                                voteDTO1.setAnswerOwner(pageDTOAns.getEntries().get(ansNumber - 1).getAuthor());
+                                                voteDTO1.setVoterId(userDTO.getId());
+                                                answerService.voteAnswer(voteDTO1);
+                                                break;
+                                            case 3:  // Report
+                                                answerService.reportAnswer(pageDTOAns.getEntries().get(ansNumber - 1).getId(), true);
+                                                break;
+                                            case 4:  // go back
+                                                return;
+                                        }
+                                        break;
+                                    case 2:  // go to the next page
+                                        if (pageDTOAns.getCounter() == Constants.PAGE_SIZE)
+                                            pageAns++;
+                                        else
+                                            mainView.showMessage("!!!! THIS IS THE LAST PAGE !!!!");
+                                        break;
+                                    case 3:  // go to the previous page
+                                        if (pageAns > 1)
+                                            pageAns--;
+                                        else
+                                            mainView.showMessage("!!!! THIS IS THE FIRST PAGE !!!!");
+                                        break;
+                                    case 4:  // go back
+                                        return;
+                                }
+                                break;
+                            case 3: // delete question
+                                questionService.deleteQuestion(questionPageDTO.getId());
+                                break;
+                            case 4: // update question
+                                QuestionModifyDTO questionModifyDTO = new QuestionModifyDTO();
+                                questionModifyDTO.setId(questionPageDTO.getId());
+                                questionView.modifyQuestion(questionModifyDTO);
+                                questionService.updateQuestion(questionModifyDTO);
+                                break;
+                            case 5: // go back
+                                return;
+                        }
+                    case 2: // go to the next page
+                        if (pageDTO.getCounter() == Constants.PAGE_SIZE)
+                            page++;
+                        else
+                            mainView.showMessage("!!!! THIS IS THE LAST PAGE !!!!");
+                        break;
+                    case 3: // go ot the previous page
+                        if (page > 1)
+                            page--;
+                        else
+                            mainView.showMessage("!!!! THIS IS THE FIRST PAGE !!!!");
+                        break;
+                    case 4: // go back
+                        return;
+                }
+            } while (true);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
 
     // menu se logged oppure no
     // operazioni varie anche tutto in un solo metodo se basta
