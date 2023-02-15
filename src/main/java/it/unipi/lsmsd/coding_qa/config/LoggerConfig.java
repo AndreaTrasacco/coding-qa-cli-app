108548be62b0a0c9bb845115a18b6990bf879a28
Andrea1900 <66999436+Andrea1900@users.noreply.github.com>
2023-02-15 16:54:53 +0100

    Little corrections

--- src/main/java/it/unipi/lsmsd/coding_qa/config/LoggerConfig.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/config/LoggerConfig.java
@@ -1,7 +1,8 @@
 package it.unipi.lsmsd.coding_qa.config;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggerConfig {
-    Logger logger = LoggerFactory.getLogger("coding_qa");
+    public static Logger logger = LoggerFactory.getLogger("coding_qa");
 }
--- src/main/java/it/unipi/lsmsd/coding_qa/dao/neo4j/SuggestionsNeo4JDAO.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/dao/neo4j/SuggestionsNeo4JDAO.java
@@ -21,7 +21,7 @@ public class SuggestionsNeo4JDAO extends BaseNeo4JDAO implements SuggestionsDAO 
         String suggestionQuery = "MATCH (startUserQuestion:Question) <- [:CREATED]-(startUser:User{ nickname : $nickname}) " +
                 "WHERE startUserQuestion.closed = false " +
                 "WITH DISTINCT(startUserQuestion.topic) AS topics, startUser " +
-                "MATCH p = (startUser)-[*1..2]->(followed:User)-[:CREATED]->(q2:Question) " +
+                "MATCH p = (startUser)-[:FOLLOW *1..3]->(followed:User)-[:CREATED]->(q2:Question) " +
                 "WHERE q2.closed = true AND q2.topic IN topics " +
                 "RETURN DISTINCT q2.id AS id, q2.createdDate AS createdDate, q2.title AS title, q2.topic AS topic, followed.nickname AS nickname, length(p) as depth " +
                 "ORDER BY depth ASC, createdDate DESC " +
@@ -33,8 +33,8 @@ public class SuggestionsNeo4JDAO extends BaseNeo4JDAO implements SuggestionsDAO 
 
     // method for suggesting questions that the user might be able to answer
     public PageDTO<QuestionDTO> questionsToAnswer(int page, String nickname) throws DAONodeException {
-        String suggestionQuery = "MATCH (u1:User{nickname : $nickname})-[:ANSWERED]->(q1)<-[:CREATED]-(u2:User)-[:CREATED]->(q2:Question{closed: false})<-[a:ANSWERED]-() " +
-                "RETURN q2.id AS id, q2.createdDate AS createdDate, q2.title AS title, q2.topic AS topic, u2.nickname AS nickname, u2, COUNT(a) AS ans_count " +
+        String suggestionQuery = "MATCH (u1:User{nickname : $nickname})-[:ANSWERED]->(q1)<-[:CREATED]-(u2:User)-[:CREATED]->(q2:Question{closed: false}), p=(q2)<-[:ANSWERED *0..1]-() " +
+                "RETURN q2.id AS id, q2.createdDate AS createdDate, q2.title AS title, q2.topic AS topic, u2.nickname AS nickname, (COUNT(p) - 1) AS ans_count " +
                 "ORDER BY ans_count, createdDate DESC " +
                 "SKIP $toSkip " +
                 "LIMIT $toLimit ";
