61e21798d0134e03eb8df1e52816d1f5dbeb3283
Andrea1900 <66999436+Andrea1900@users.noreply.github.com>
2023-02-15 19:27:41 +0100

    Changed URI MongoDB

--- src/main/java/it/unipi/lsmsd/coding_qa/dao/base/BaseMongoDBDAO.java
+++ src/main/java/it/unipi/lsmsd/coding_qa/dao/base/BaseMongoDBDAO.java
@@ -8,7 +8,7 @@ import com.mongodb.client.*;
 
 public abstract class BaseMongoDBDAO {
     protected static MongoClient mongoClient;
-    private final static String URI = "mongodb://10.1.1.15:27020,10.1.1.16:27020,10.1.1.17:27020/?retryWrites=true&w=1&wtimeout=10000&readPreference=nearest";
+    private final static String URI = "mongodb://10.1.1.15:27020,10.1.1.16:27020,10.1.1.17:27020/?retryWrites=true&w=1&wtimeout=10000&readPreference=secondaryPreferred";
     protected static final String DB_NAME = "codingqa";
 
     public static MongoClient getConnection() {
@@ -21,7 +21,7 @@ public abstract class BaseMongoDBDAO {
 
     public static TransactionOptions txnOptions = TransactionOptions.builder()
             .readPreference(ReadPreference.primary())
-            .writeConcern(WriteConcern.MAJORITY)
+            .writeConcern(WriteConcern.W1)
             .build();
 
     public static void close() {
